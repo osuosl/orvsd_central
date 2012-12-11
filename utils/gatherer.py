1e385cea76e2e790a1126890ffddc299a8838bb4
Justin Noah <jnoah@osuosl.org>
2012-12-11 15:23:55 -0800

    gather: update/fix, still need to do a bit to work with our db

--- utils/gatherer.py
+++ utils/gatherer.py
@@ -16,8 +16,6 @@ db_sites = []
 find = "SELECT table_schema, table_name FROM information_schema.tables WHERE table_name = 'siteinfo' OR table_name = 'mdl_siteinfo';"
 curs.execute(find)
 check = curs.fetchall()
-
-# Done getting stuff from applegate
 con.close()
 
 # store the db names and table name in an array to sift through
@@ -31,14 +29,21 @@ if len(check):
 
     # Do all the works
     for db in db_sites:
+        # Connect to the new db
+        cherry = connect(user=user, passwd=pwrd, host=address, db=db[0])
+        pie = cherry.cursor()
+
         # Grab the site info data
-        curs.execute("select * from `%s`;" % db[1])
-        data = curs.fetchall()
+        pie.execute("select * from `%s`;" % db[1])
+        data = pie.fetchall()
 
         # For all the data, shove it into the central db
         for d in data:
            c_curs.execute("INSERT INTO siteinfo (baseurl, basepath, sitename, sitetype, siteversion, siterelease, adminemail, totalusers, adminusers, teachers, activeusers, totalcourses, courses, timemodified) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);", d[1:])
 
-    central.close()
+        # And finally close cherry pie
+        cherry.close()
 
+    # Finished with central, closing up
+    central.close()
 
