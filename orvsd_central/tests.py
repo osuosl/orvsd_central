f59402c08ba938d80889859355feb5d3662c3ff5
Dean Johnson <johnsdea@osuosl.org>
2012-12-13 13:29:10 -0800

    Changed name of package from site to orvsd_central.
    refs #12489

---
+++ orvsd_central/tests.py
@@ -0,0 +1,19 @@
+from orvsd_central import main
+import unittest
+import tempfile
+import sqlalchemy
+
+class ORVSD_Central_Tests(unittest.TestCase):
+    def add_User(username, password, confirm_password, email, permission_level):    
+        return self.app.post('/register', data=dict(
+            username=username,
+            password=password,
+            confirm_password=confirm_password,
+            email=email,
+            permission_level=permission_level
+        ))
+
+    def test_add_User():
+        username = 'name'
+        resp = self.add_User(user, 'password', 'password', 'confirm_password', 'email@orvsd.org', 'Admin')
+        assert user+' has been added to the database successfully!' in resp.data
