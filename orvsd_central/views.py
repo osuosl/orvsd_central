c71c55bf83c8c30d25fd4156cd879ee02142bdd5
Josh Deare <dearej@osuosl.org>
2013-03-07 18:51:46 -0800

    SUCCESS!!! oauth2 now successfully works gets an access token, then queries google for an email address checks that address against our db, then logs the user in if they are in the db then tosses the access_token from session for security refs #12837

--- orvsd_central/views.py
+++ orvsd_central/views.py
@@ -5,16 +5,13 @@ from flask.ext.login import (login_required, login_user, logout_user,
 from werkzeug import check_password_hash, generate_password_hash
 from orvsd_central import db, app, login_manager, google
 from forms import LoginForm, AddDistrict, AddSchool, AddUser, InstallCourse
-<<<<<<< HEAD
 from models import (District, School, Site, SiteDetail,
                     Course, CourseDetail, User)
 from sqlalchemy import func
 from sqlalchemy.sql.expression import desc
-=======
 from models import District, School, Site, SiteDetail, Course, CourseDetail, User
 import urllib2
->>>>>>> got google login with access token working
-
+import json
 import re
 import subprocess
 import StringIO
@@ -56,7 +53,6 @@ def login():
     return render_template("login.html", form=form)
 
 
-<<<<<<< HEAD
 def get_user():
     # A user id is sent in, to check against the session
     # and based on the result of querying that id we
@@ -67,36 +63,41 @@ def get_user():
             return User.query.filter_by(id=session["user_id"]).first()
     return None
 
-=======
+
 @app.route("/google_login")
 def google_login():
     print "before token"
+
+
+def google_login():    
     access_token = session.get('access_token')
-    print 'after token'
     if access_token is None:
         callback=url_for('authorized', _external=True)
         return google.authorize(callback=callback)
-        print 'after google request'
     else:
-        access_token = access_token[0]
-        print "before header creation"
-        print access_token
-        headers = {'Authorization': 'Oauth '+access_token}
-        print 'before Request'
+        access_token = access_token
+        headers = {'Authorization': 'OAuth '+access_token}
         req = urllib2.Request('https://www.googleapis.com/oauth2/v1/userinfo',
                                 None, headers)
-        print 'before open url'
         try:
             res = urllib2.urlopen(req)
         except urllib2.URLError, e:
             if e.code == 401:
-                print "token error"
                 session.pop('access_token', None)
                 flash('There was a problem with your Google login information.  Please try again.')
                 return redirect(url_for('login'))
             return res.read()
-        return res.read()
-
+        obj = json.loads(res.read())
+        email = obj['email']
+        user = User.query.filter_by(email=email).first()
+        #pop access token so it isn't sitting around in our session any longer than nescessary
+        session.pop('access_token', None)
+        if user is not None:
+            login_user(user)
+            return redirect(url_for('report'))
+        else:
+            flash("This google account was not recognized as having access. Sorry.")
+            return redirect(url_for('login'))
 
 @app.route(app.config['REDIRECT_URI'])
 @google.authorized_handler
@@ -108,7 +109,7 @@ def authorized(resp):
 @google.tokengetter
 def get_access_token():
     return session.get('access_token')
->>>>>>> got google login with access token working
+
 
 @app.route("/logout")
 def logout():
