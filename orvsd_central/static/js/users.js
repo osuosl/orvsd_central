9e73a1bfdc296874ade80ed3a0ae4c2d8041396a
Dean Johnson <johnsdea@osuosl.org>
2013-04-05 11:00:37 -0700

    users: Updated to using data tags in the template.
    refs #12357

--- orvsd_central/static/js/users.js
+++ orvsd_central/static/js/users.js
@@ -1,7 +1,7 @@
 $(function() {
     $(".user").on("click", function() {
-        var user_id = $(this).attr("id");
-        var user_name = $(this).attr("name");
+        var user_id = $(this).attr("data-user_id");
+        var user_name = $(this).attr("data-user_name");
         var confirmed = confirm("Are you sure you want to delete " + user_name + "?");
 
         if (confirmed) {
