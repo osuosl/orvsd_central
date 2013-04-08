1fbcc9ba8868bcbd1f9dafec929d400d90fee88a
Josh Deare <dearej@osuosl.org>
2013-04-08 15:06:19 -0700

    pep8'd __init__.py refs#12837

--- orvsd_central/__init__.py
+++ orvsd_central/__init__.py
@@ -15,22 +15,26 @@ login_manager.setup_app(app)
 
 
 oauth = OAuth()
-google = oauth.remote_app('google',
-                            base_url = 'https://www.google.com/accounts/',
-                            authorize_url='https://accounts.google.com/o/oauth2/auth',
-                            request_token_url=None,
-                            request_token_params={'scope': 'https://www.googleapis.com/auth/userinfo.email', 'response_type': 'code'},
-                            access_token_url='https://accounts.google.com/o/oauth2/token',
-                            access_token_method='POST',
-                            access_token_params={'grant_type': 'authorization_code'},
-                            consumer_key=app.config['GOOGLE_CLIENT_ID'],
-                            consumer_secret=app.config['GOOGLE_CLIENT_SECRET'])
-                
+google = oauth.remote_app(
+    'google',
+    base_url='https://www.google.com/accounts/',
+    authorize_url='https://accounts.google.com/o/oauth2/auth',
+    request_token_url=None,
+    request_token_params={
+        'scope':
+        'https://www.googleapis.com/auth/userinfo.email', 'response_type':
+        'code'},
+    access_token_url='https://accounts.google.com/o/oauth2/token',
+    access_token_method='POST',
+    access_token_params={'grant_type': 'authorization_code'},
+    consumer_key=app.config['GOOGLE_CLIENT_ID'],
+    consumer_secret=app.config['GOOGLE_CLIENT_SECRET'])
 
 
-import models, views
+import models
+import views
+
 
 @app.before_request
 def before_request():
     g.db = db
-
