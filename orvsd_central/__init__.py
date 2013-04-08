5784f7ddba345b0e99514428378bc8e2f5d3b33c
Josh Deare <dearej@osuosl.org>
2013-04-08 15:06:19 -0700

    pep8'd __init__.py refs#12837

--- orvsd_central/__init__.py
+++ orvsd_central/__init__.py
@@ -14,27 +14,33 @@ login_manager = LoginManager()
 login_manager.setup_app(app)
 
 
-<<<<<<< HEAD
 import models
 import views
 
-=======
+
 oauth = OAuth()
-google = oauth.remote_app('google',
-                            base_url = 'https://www.google.com/accounts/',
-                            authorize_url='https://accounts.google.com/o/oauth2/auth',
-                            request_token_url=None,
-                            request_token_params={'scope': 'https://www.googleapis.com/auth/userinfo.email', 'response_type': 'code'},
-                            access_token_url='https://accounts.google.com/o/oauth2/token',
-                            access_token_method='POST',
-                            access_token_params={'grant_type': 'authorization_code'},
-                            consumer_key=app.config['GOOGLE_CLIENT_ID'],
-                            consumer_secret=app.config['GOOGLE_CLIENT_SECRET'])
-                
+google = oauth.remote_app(
+    'google',
+    base_url='https://www.google.com/accounts/',
+    authorize_url='https://accounts.google.com/o/oauth2/auth',
+    request_token_url=None,
+    request_token_params={
+        'scope':
+        'https://www.googleapis.com/auth/userinfo.email', 'response_type':
+        'code'},
+    access_token_url='https://accounts.google.com/o/oauth2/token',
+    access_token_method='POST',
+    access_token_params={'grant_type': 'authorization_code'},
+    consumer_key=app.config['GOOGLE_CLIENT_ID'],
+    consumer_secret=app.config['GOOGLE_CLIENT_SECRET'])
+
+
+import models
+import views
 
 
 import models, views
->>>>>>> got google login with access token working
+
 
 @app.before_request
 def before_request():
