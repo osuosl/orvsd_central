1e4737ab9517de11ddb3c2d05e22898a2caec5b4
Josh Deare <dearej@osuosl.org>
2013-04-29 13:21:25 -0700

    updated for changes documents by brutal_chaos documented code, removed decorator from view_school updated requirements.txt load is an extra layer of granularity that I added for future features on request of Rudy, currently helpdesk and admin should have the same perms refs #13293

--- orvsd_central/views.py
+++ orvsd_central/views.py
@@ -31,15 +31,17 @@ def no_perms():
 def on_identity_loaded(sender, identity):
     identity.user = current_user
 
+    # adds the current user as a UserNeed to our current permission
     if hasattr(current_user, 'id'):
         identity.provides.add(UserNeed(current_user.id))
-
+    # adds permissions depending on the role a user has
+    # 3 is steve, 2 is helpdesk, 1 is admin
     if hasattr(current_user, 'role'):
-        if(current_user.role < 3):
+        if(current_user.role < 3):  # give steve view permission
             identity.provides.add(RoleNeed('view'))
-        if(current_user.role < 2):
+        if(current_user.role < 2):  # give helpdesk add permission
             identity.provides.add(RoleNeed('add'))
-        if(current_user.role < 1):
+        if(current_user.role < 1):  # give admin load permission
             identity.provides.add(RoleNeed('load'))
 
 
@@ -204,7 +206,6 @@ def add_course():
 
 
 @login_required
-@view_permission.require()
 def view_school(school):
     # Info for the school's page
     admins = 0
