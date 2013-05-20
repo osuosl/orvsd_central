d12fe9e9265b3b2f20684aad1f6a3802ec4e7401
Justin Noah <jnoah@osuosl.org>
2013-05-20 12:29:23 -0700

    install_course: show the address of the selectid site
    refs #12693

--- orvsd_central/static/js/install_course.js
+++ orvsd_central/static/js/install_course.js
@@ -1,4 +1,11 @@
 $(function() {
+    $('#site').change(function() {
+        var site_id = $('#site option:selected').val();
+        $.get('/get_site_by/' + site_id, function(data) {
+            $('#address').html("Address: <i>" + data.address + "</i>");
+        });
+    });
+
     $('#course').change(function() {
         var html = "";
         $('#course option:selected').each(function() {
