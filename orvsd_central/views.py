a839f3aefa972b6cda6cfa18a9feca8882d87a01
Dean Johnson <johnsdea@osuosl.org>
2013-07-16 17:59:13 -0700

    celery: Fixed config and implemented tasks.
    To run celery tasks execute the following command from the
    top level directory of orvsd_central:
    >> celery -A tasks worker -l info
    
    refs #13839

--- orvsd_central/views.py
+++ orvsd_central/views.py
@@ -12,7 +12,11 @@ from sqlalchemy import func, and_
 from sqlalchemy.sql.expression import desc
 from models import (District, School, Site, SiteDetail,
                     Course, CourseDetail, User)
+<<<<<<< HEAD
 import celery
+=======
+from tasks import celery
+>>>>>>> celery: Fixed config and implemented tasks.
 import json
 import re
 import subprocess
@@ -286,6 +290,7 @@ def install_course():
                                 output=output,
                                 user=current_user)
 
+
 @celery.task(name='tasks.install_course')
 def install_course_to_site(course, site):
     # To get the file path we need the text input, the lowercase of
@@ -308,10 +313,14 @@ def install_course_to_site(course, site):
 
     resp = requests.post(site, data=data)
 
+<<<<<<< HEAD
     # Unfortunately the response object to turned into unicode
     # when returned by the celery job, so we must send the
     # data we need, instead of the whole object.
     return resp.text
+=======
+    return "%s\n\n%s\n\n\n" % (course.course.shortname, resp.text)
+>>>>>>> celery: Fixed config and implemented tasks.
 """
 VIEW
 """
