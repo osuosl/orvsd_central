6541840e4ca0ed8fec7259d31ba2b388ac4c2a94
Dean Johnson <johnsdea@osuosl.org>
2014-02-10 09:15:39 -0800

    Renamed objects to districts.
    More clear than before.
    
    refs #14745

--- orvsd_central/views.py
+++ orvsd_central/views.py
@@ -673,19 +673,19 @@ def get_access_token():
     return session.get('access_token')
 
 
-def build_accordion(objects, accordion_id, type, extra=None):
+def build_accordion(districts, accordion_id, type, extra=None):
     inner_t = app.jinja_env.get_template('accordion_inner.html')
     outer_t = app.jinja_env.get_template('accordion.html')
 
     inner = ""
 
-    for obj in objects:
-        inner_id = re.sub(r'[^a-zA-Z0-9]', '', obj.shortname)
+    for district in districts:
+        inner_id = re.sub(r'[^a-zA-Z0-9]', '', district.shortname)
         inner += inner_t.render(accordion_id=accordion_id,
                                 inner_id=inner_id,
                                 type=type,
-                                link=obj.name,
-                                extra=None if not extra else extra % obj.id)
+                                link=district.name,
+                                extra=None if not extra else extra % district.id)
 
     return outer_t.render(accordion_id=accordion_id,
                           dump=inner)
