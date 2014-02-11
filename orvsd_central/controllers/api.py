dc8d126e3ce7f334e4e824f8560afea58902017c
Justin Noah <jnoah@osuosl.org>
2014-02-11 15:18:11 -0800

    api: moved get_course_list
    refs #14505

--- orvsd_central/controllers/api.py
+++ orvsd_central/controllers/api.py
@@ -1,8 +1,33 @@
 from flask import Blueprint, jsonify
 
-from orvsd_central.models import Site
+from orvsd_central import db
+from orvsd_central.models import Course, CourseDetail, Site
 
 mod = Blueprint('api', __name__)
+
+
+@mod.route("/courses/filter", methods=["POST"])
+def get_course_list():
+    dir = request.form.get('filter')
+
+    if dir == "None":
+        courses = CourseDetail.query.all()
+    else:
+        courses = db.session.query(CourseDetail).join(Course) \
+                    .filter(Course.source == dir).all()
+
+    # This means the folder selected was not the source folder or None.
+    if not courses:
+        courses = db.session.query(CourseDetail).filter(CourseDetail.filename
+                                                        .like("%"+dir+"%"))\
+                                                .all()
+
+    courses = sorted(courses, key=lambda x: x.course.name)
+
+    serialized_courses = [{'id': course.course_id,
+                           'name': course.course.name}
+                          for course in courses]
+    return jsonify(courses=serialized_courses)
 
 
 @mod.route('/get_site_by/<int:site_id>', methods=['GET'])
--- orvsd_central/views.py
+++ orvsd_central/views.py
@@ -142,29 +142,6 @@ def install_course():
                                user=current_user)
 
 
-@app.route("/courses/filter", methods=["POST"])
-def get_course_list():
-    dir = request.form.get('filter')
-
-    if dir == "None":
-        courses = CourseDetail.query.all()
-    else:
-        courses = db.session.query(CourseDetail).join(Course) \
-                    .filter(Course.source == dir).all()
-
-    # This means the folder selected was not the source folder or None.
-    if not courses:
-        courses = db.session.query(CourseDetail).filter(CourseDetail.filename
-                                                        .like("%"+dir+"%"))\
-                                                .all()
-
-    courses = sorted(courses, key=lambda x: x.course.name)
-
-    serialized_courses = [{'id': course.course_id,
-                           'name': course.course.name}
-                          for course in courses]
-    return jsonify(courses=serialized_courses)
-
 
 def get_course_folders():
     base_path = "/data/moodle2-masters/"
