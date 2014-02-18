c0762dfa514f9c2901e2706da8a7af8558441e16
Justin Noah <jnoah@osuosl.org>
2014-02-18 15:19:41 -0800

    __init__: pep8
    refs #15831

--- orvsd_central/__init__.py
+++ orvsd_central/__init__.py
@@ -9,15 +9,19 @@ app.config.from_object('config')
 login_manager = LoginManager()
 login_manager.setup_app(app)
 
+
 def init_celery(app):
     celery = Celery(app.import_name, broker=app.config['CELERY_BROKER_URL'])
     celery.conf.update(app.config)
     TaskBase = celery.Task
+
     class ContextTask(TaskBase):
         abstract = True
+
         def __call__(self, *args, **kwargs):
             with app.app_context():
                 return TaskBase.__call__(self, *args, **kwargs)
+
     celery.Task = ContextTask
     return celery
 
@@ -38,7 +42,6 @@ google = oauth.remote_app(
     access_token_params={'grant_type': 'authorization_code'},
     consumer_key=app.config['GOOGLE_CLIENT_ID'],
     consumer_secret=app.config['GOOGLE_CLIENT_SECRET'])
-
 
 
 @app.errorhandler(404)
