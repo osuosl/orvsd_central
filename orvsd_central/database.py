4eff316a2d41e1923a7cc2616b6222abf315db4c
Justin Noah <jnoah@osuosl.org>
2014-02-19 14:58:47 -0800

    database: use the app, not current_app
    refs #15831

--- orvsd_central/database.py
+++ orvsd_central/database.py
@@ -1,10 +1,11 @@
-from flask import current_app
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
+from orvsd_central import app
+
 # Get the db address from the current app
-_db_address = current_app.config['SQLALCHEMY_DB_URI']
+_db_address = app.config['SQLALCHEMY_DATABASE_URI']
 
 engine = create_engine(_db_address, convert_unicode=True)
 db_session = scoped_session(sessionmaker(autocommit=False,
