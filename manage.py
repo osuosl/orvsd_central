7896fbe6e469266fca87a7c1866e73d8c65e4a5c
Justin Noah <jnoah@osuosl.org>
2014-02-21 11:11:51 -0800

    initdb: command added to manage.py
    refs #15699

--- manage.py
+++ manage.py
@@ -4,5 +4,12 @@ from orvsd_central import app
 
 manager = Manager(app)
 
+
+@manager.command
+def initdb():
+    from orvsd_central.database import init_db
+    init_db()
+
+
 if __name__ == '__main__':
     manager.run()
