3495a9a8f6c7925ef6e9f2957abf780887e6361e
Dean Johnson <johnsdea@osuosl.org>
2014-04-21 22:01:33 +0000

    Fixed scoping issue and added delete UX fix.
    Previously, when an object was deleted, the next option was not
    auto-populated into the old fields. This has since been corrected.
    
    refs #16071

--- orvsd_central/static/js/update_models.js
+++ orvsd_central/static/js/update_models.js
@@ -2,17 +2,18 @@ $(document).on("ready", function() {
     // [1] will skip the first /, which returns an empty string and
     // give the category we are looking for.
     var category = window.location.pathname.split("/")[1]
+    var pairs;
+
+    pairs = display_obj($("#object_list option:selected"), category);
 
     // Stores the current keys and vals for the object we are looking at.
-    var pairs = display_obj($("#object_list option:selected"), category);
-    var url;
 
     $("#object_list").on("change", function() {
         pairs = display_obj($(this), category);
     });
 
     $("#add").on("click", function() {
-        url = "/" + category;
+        var url = "/" + category;
         $.get(url + "/keys", function(resp) {
             var rows = "";
             // Generate a new form with keys corresponding to a Model's
@@ -43,33 +44,46 @@ $(document).on("ready", function() {
         else {
             // Posts to /update or /delete, depending on the button that
             // was clicked.
-            method = $(this).attr("name");
-            $.post(url + "/" + method, data).done(function(resp) {
-                // If we are deleting an object, we should clear all
-                // references to that data.
+            var method = $(this).attr("name");
+            var url = "/" + category + "/" + data['id'] + "/" + method;
+            $.post(url, data).done(function(resp) {
                 if (method === "delete") {
-                    $("#form").empty();
+                    // Remove all references to old object
+                    $("#form").empty();$
+
+                    // Record the next object before we delete the current.
+                    var next = $('#object_list option:selected').next()
+
                     $("#object_list option:selected").remove();
+                    next.attr('selected', 'selected');
+
+                    // Edge case for last element
+                    if (next.val() === undefined) {
+                        next = $("#object_list option:selected");
+                    }
+                    pairs = display_obj(next, category);
                 }
                 $("#message").html(resp);
             });
         }
     });
-});
 
-function display_obj(obj, category) {
-    var url = "/" + category + "/" + obj.val();
-    $.get(url, function(resp) {
-        var rows = "";
-        for (var key in resp) {
-            rows += generate_row(key, resp[key]);
-        }
-        $("#form").empty();
-        $("#form").html(rows);
-        $("#message").empty();
-        return resp;
-    });
-}
+    function display_obj(obj, category) {
+        var url = "/" + category + "/" + obj.val();
+        $.get(url, function(resp) {
+            var rows = "";
+            for (var key in resp) {
+                rows += generate_row(key, resp[key]);
+            }
+            $("#form").empty();
+            $("#form").html(rows);
+            $("#message").empty();
+            pairs = resp
+        });
+        // Return keys and vals for the object we recieved.
+        return pairs;
+    }
+});
 
 // Used to generate the html for an input field for an attribute.
 function generate_row(key, value) {
