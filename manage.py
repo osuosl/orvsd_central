f364caf91d71642c8b93ad32fe130a32431f2f00
Dean Johnson <johnsdea@osuosl.org>
2014-06-04 21:08:17 +0000

    Added some documentation for manage.
    refs #16299

--- manage.py
+++ manage.py
@@ -5,17 +5,28 @@ from orvsd_central import attach_blueprints, create_app
 from orvsd_central.database import create_db_session, init_db
 
 def setup_app(config=None):
+    """
+    Creates an instance of our application.
+
+    We needed to create the app in this way so we could set different
+    configurations within the same app. (hint: not using the same database
+    when testing)
+    """
     app = create_app(config) if config else create_app()
     with app.app_context():
         g.db_session = create_db_session()
         attach_blueprints()
         return app
 
+# Setup our manager
 manager = Manager(setup_app)
 manager.add_option('-c', '--config', dest='config')
 
 @manager.command
 def gather():
+    """
+    Runs the gather_siteinfo script to generate SiteDetails.
+    """
     with current_app.app_context():
         from orvsd_central.util import gather_siteinfo
         g.db_session = create_db_session()
@@ -23,6 +34,12 @@ def gather():
 
 @manager.command
 def initdb():
+    """
+    Sets up the schema for a database that already exists (MySQL, Postgres) or
+    creates the database (SQLite3) outright.
+    * This also includes an option to create a new user, but that won't work
+      on an in-memory database.
+    """
     with current_app.app_context():
         g.db_session = create_db_session()
         init_db()
