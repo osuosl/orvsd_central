247bc86282a43fa1757c38077f9a02fa4cb4c90b
Dean Johnson <johnsdea@osuosl.org>
2014-06-04 21:20:55 +0000

    Added some comments for migrate.js
    This file should be cleaned up, it looks like a tornado hit it.
    
    refs #16299

--- orvsd_central/static/js/migrate.js
+++ orvsd_central/static/js/migrate.js
@@ -1,7 +1,9 @@
 $(document).on("ready", function() {
+    // Move the selected school to the selected district.
     $("#update").on("click", function() {
         var url = "/schools" ;
         $.get(url + "/" + $("#schools option:selected").val(), function(resp) {
+            // Update the school with the new district id.
             resp['district_id'] = $("#districts option:selected").val();
             $.post(url + "/" + $("#schools option:selected").val() + "/update", resp).done(function(msg) {
                 // If message is blank there was a 404.
