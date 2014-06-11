051f01984b9210a18069997bf5818851d4e0c328
Justin Noah <jnoah@osuosl.org>
2014-06-11 09:35:09 -0700

    package: fixup setup.py
    refs #15717

--- setup.py
+++ setup.py
@@ -7,9 +7,14 @@ setup(
     name="orvsd-central",
     description="The central hub for ORVSD",
     long_description=open('README.txt').read(),
-    version="0.1.0",
+    version="0.0.1",
     url="https://orvsd.org",
     author="OSUOSL",
     author_email="support@osuosl.org",
     packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'orvsd-central = orvsd_central.wsgi:app',
+        ],
+    },
 )
