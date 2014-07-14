e3eb84b45e2a7bf47301d1f59b67823bf6a8c57f
Justin Noah <jnoah@osuosl.org>
2014-07-14 14:00:04 -0700

    migrate: allow multiple school selections
    refs #9

--- orvsd_central/static/js/migrate.js
+++ orvsd_central/static/js/migrate.js
@@ -2,16 +2,19 @@ $(document).on("ready", function() {
     // Move the selected school to the selected district.
     $("#update").on("click", function() {
         var url = "/schools" ;
-        $.get(url + "/" + $("#schools option:selected").val(), function(resp) {
-            // Update the school with the new district id.
-            resp['district_id'] = $("#districts option:selected").val();
-            $.post(url + "/" + $("#schools option:selected").val() + "/update", resp).done(function(msg) {
-                // If message is blank there was a 404.
-                if (msg != "") {
-                    $("#message").html("Migrated " + $("#schools option:selected").text() + " to " +
-                        $("#districts option:selected").text() + "!");
-                }
+        $("#schools option:selected").each(function) {
+            $.get(url + "/" + $(this).val(), function(resp) {
+                // Update the school with the new district id.
+                resp['district_id'] = $("#districts option:selected").val();
+                $.post(url + "/" + $(this).val() + "/update", resp).done(function(msg) {
+                    // If message is blank there was a 404.
+                    if (msg != "") {
+                        $("#message").html("Migrated " + $(this).text() + " to " +
+                            $("#districts option:selected").text() + "!");
+                    }
+                });
             });
         });
+
     });
 });
