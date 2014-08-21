4cdb002727b9c5d37e2ac986edb16a4203d6c532
Justin Noah <jnoah@osuosl.org>
2014-08-21 10:22:49 -0700

    test base: handle app context

--- tests/base.py
+++ tests/base.py
@@ -1,10 +1,11 @@
 import unittest
 
+from flask import g
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 
 import orvsd_central
-from orvsd_central import models
+from orvsd_central import attach_blueprints, models
 from orvsd_central.database import Model
 
 
@@ -16,24 +17,32 @@ class TestBase(unittest.TestCase):
         self.db_session and self.app respectively.
         """
 
-        self.test_engine = create_engine("sqlite:///:memory:",
-                                         convert_unicode=True)
-        self.db_session = scoped_session(sessionmaker(autocommit=True,
-                                                      autoflush=True,
-                                                      bind=self.test_engine))
-        Model.query = self.db_session.query_property()
-
-        # Create the tables
-        Model.metadata.create_all(bind=self.test_engine)
-
         # Create the app
-        self.app = orvsd_central.app.test_client()
+        self.app = orvsd_central.create_app()
         self.app.testing = True
+
+        with self.app.app_context():
+            attach_blueprints()
+
+            self.test_engine = create_engine(
+                "sqlite:///:memory:",
+                 convert_unicode=True
+            )
+            g.db_session = scoped_session(
+                sessionmaker(
+                    autocommit=True,
+                    autoflush=True,
+                    bind=self.test_engine
+                )
+            )
+            Model.query = g.db_session.query_property()
+
+            # Create the tables
+            Model.metadata.create_all(bind=self.test_engine)
 
     def tearDown(self):
         """
         Close the session and clean the db. Some tests may create
         data and we don't want to keep that around
         """
-        self.db_session.remove()
         Model.metadata.drop_all(bind=self.test_engine)
