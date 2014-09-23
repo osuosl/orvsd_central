7eeea3a22ef58fc0f5adae2d9794aa32a3e050a3
Justin Brown <brownju@cyan.workstation.osuosl.bak>
2014-09-23 10:48:43 -0700

    added TODO

--- orvsd_central/database.py
+++ orvsd_central/database.py
@@ -32,7 +32,10 @@ def create_admin_account(silent):
     config: Bool - use config vars
     """
 
+<<<<<<< HEAD
     if silent:
+        # TODO: Check if there really are no admin accounts?
+
         # Create an admin account.
         ans = raw_input("There are currently no admin accounts, would you like to "
                         "create one? (Y/N) ")
