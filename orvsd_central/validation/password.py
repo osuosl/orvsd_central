a30073d7404bee8a5b57dd849caa5be385a15e21
Justin Brown <brownju@diamond.workstation.osuosl.bak>
2014-10-02 15:13:07 -0700

    Adding validation module. +more
    Added validation module and removed is_valid_string function one-liner
    Changed imports in controllers/general.py and database.py for new module
    Changed email regex back to previous version after talking to deanj about it.

---
+++ orvsd_central/validation/password.py
@@ -0,0 +1,28 @@
+# Defines functions for validating user inputs as unique/valid
+
+from getpass import getpass
+
+def is_valid_password(passwd):
+    """
+    Check that `passwd` is a string, and not empty.
+    """
+    return is_valid_string(passwd)
+    # Room to add more.
+
+def get_matching_passwords():
+    """
+    Returns valid matching passwords from the prompt.
+    """
+    matching = False
+    while not matching:
+        passwd = getpass("Password: ")
+        if not passwd:
+            print("Please enter a password.")
+            continue
+
+        confirm = getpass("Confirm: ")
+        matching = passwd == confirm
+        if not matching:
+            print("Passwords do not match. Please try again.")
+
+    return passwd
