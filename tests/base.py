7d3a3eeb6a5e107cb2afcea03672ac65361eb597
Justin Noah <jnoah@osuosl.org>
2015-01-26 19:34:53 -0800

    remove old context and database handler
    refs #111

--- tests/base.py
+++ tests/base.py
@@ -2,11 +2,8 @@ import unittest
 
 from contextlib import contextmanager
 from flask import appcontext_pushed, g
-from sqlalchemy import create_engine
-from sqlalchemy.orm import scoped_session, sessionmaker
 
 import orvsd_central
-from orvsd_central import attach_blueprints
 from orvsd_central.database import create_db_session
 from orvsd_central.models import Model
 
@@ -40,29 +37,3 @@ class TestBase(unittest.TestCase):
         # Create the app
         self.app = orvsd_central.create_app(config_changes=cfg)
         self.app.testing = True
-
-        with self.app.app_context():
-            attach_blueprints()
-
-            self.test_engine = create_engine(
-                "sqlite:///:memory:",
-                convert_unicode=True
-            )
-            g.db_session = scoped_session(
-                sessionmaker(
-                    autocommit=True,
-                    autoflush=True,
-                    bind=self.test_engine
-                )
-            )
-            Model.query = g.db_session.query_property()
-
-            # Create the tables
-            Model.metadata.create_all(bind=self.test_engine)
-
-    def tearDown(self):
-        """
-        Close the session and clean the db. Some tests may create
-        data and we don't want to keep that around
-        """
-        Model.metadata.drop_all(bind=self.test_engine)
