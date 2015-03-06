e4a623a2ef580abc9e176da6b1ccf1989941852f
Justin Noah <jnoah@osuosl.org>
2015-03-06 14:34:05 -0800

    add migration skeleton
    refs #143

---
+++ migrations/versions/2bf8713fd2bb_moodle_tokens_default.py
@@ -0,0 +1,34 @@
+"""moodle_tokens default
+
+Revision ID: 2bf8713fd2bb
+Revises: 49e0d0a0a952
+Create Date: 2015-03-06 14:33:12.044131
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '2bf8713fd2bb'
+down_revision = '49e0d0a0a952'
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def upgrade(engine_name):
+    eval("upgrade_%s" % engine_name)()
+
+
+def downgrade(engine_name):
+    eval("downgrade_%s" % engine_name)()
+
+
+
+
+
+def upgrade_engine1():
+    pass
+
+
+def downgrade_engine1():
+    pass
+
