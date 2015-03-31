66a5fea27c3feafb75e39e7c16408b3c29dd1e12
Justin Noah <jnoah@osuosl.org>
2015-03-31 16:39:41 -0700

    Add the necessary forms
    refs #150

--- orvsd_central/forms.py
+++ orvsd_central/forms.py
@@ -31,3 +31,83 @@ class InstallCourse(Form):
     site = SelectMultipleField('Site', choices=defaults)
     course = SelectMultipleField('Course', choices=defaults)
     filter = SelectField('Filter', choices=defaults)
+
+
+class DistrictForm(Form):
+    """
+    Properties needed for a user to create a new District object
+    """
+
+    name = TextField(u"Name", validators=[Required()])
+    # We may want to have the Model autogen the shortname on __init__
+    shortname = TextField(u"Short Name", validators=[Required()])
+    base_path = TextField(u"Base Path")
+    state_id = SelectField(
+        u"Associated State", validators=[Required()], choices=defaults
+    )
+
+class SchoolForm(Form):
+    """
+    Properties needed for a user to create a new School object
+    """
+
+    name = TextField(u"Name", validators=[Required()])
+    # We may want to have the Model autogen the shortname on __init__
+    shortname = TextField(u"Short Name", validators=[Required()])
+    domain = TextField(u"Domain", validators=[Required()])
+    license = SelectField(
+        u"License",
+        validators=[Required()],
+        # This should be pulled out and possibly be managed through the config
+        # hardcoding is never OK.
+        choices=[('None', '---'), ('FLVS', 'FLVS'), ('NROC', 'NROC')],
+    )
+    county = TextField(u"County", validators=[Required()])
+    state_id = SelectField(
+        u"Associated State", validators=[Required()], choices=defaults
+    )
+
+
+class SiteForm(Form):
+    """
+    Properties needed for a user to create a new Site object
+    """
+
+    name = TextField(u"Name", validators=[Required()])
+    baseurl = TextField(u"URL", validators=[Required()])
+    basepath = TextField(u"Base Path")
+    cron = DateTimeField(u"Jenkins Cron Job")
+    location = TextField(u"Host Machine")
+    # may want to have the token field be disabled?
+    tokens = TextField(u"Moodle Tokens", default="{}")
+    school_id = SelectField(
+        u"Associated School", validators=[Required()], choices=defaults
+    )
+
+
+class CourseForm(Form):
+    """
+    Properties needed for a user to create a new District object
+    """
+
+    name = TextField(u"Name", validators=[Required()])
+    shortname = TextField(u"Short Name", validators=[Required()])
+    source = TextField(u"Source")
+    version = TextField(u"Version")
+    filename = TextField(u"Filename", validators=[Required()])
+    license = SelectField(
+        u"School License",
+        validators=[Required()],
+        # This should be pulled out and possibly be managed through the config
+        # hardcoding is never OK.
+        choices=[('None', '---'), ('FLVS', 'FLVS'), ('NROC', 'NROC')],
+    )
+    moodle_version = SelectField(
+        u"Moodle Version",
+        choices=[('Any', 'Any'), ('2.7', 2.7), ('2.5', 2.5), ('2.2', 2.2)],
+    )
+    serial = TextField(u"Serial", validators=[Required()])
+    state_id = SelectField(
+        u"Associated State", validators=[Required()], choices=defaults
+    )
+
